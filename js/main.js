08e1a295aedcd6b482a88e9438ae4a4b96513ff9
Joseph Romero <jromero361980@gmail.com>
2018-02-19 23:42:39 -0700

    add section 3-5

--- js/main.js
+++ js/main.js
@@ -5,10 +5,50 @@ $(document).ready(function() {
 	});
 });
 
-const yourInstance = new TypeIt("#element", {
+const yourInstance = new TypeIt("#element1", {
 	strings: ["designer", "developer"],
+	lifeLike: true,
 	breakLines: false,
 	nextStringDelay: 3000,
 	loopDelay: 3000,
 	loop: true
+});
+
+const secondInstance = new TypeIt("#element2", {
+	strings: ["About Me", "My Skills", "What I Do..."],
+	breakLines: false,
+	nextStringDelay: 3000,
+	loop: false,
+	startDelay: 500,
+	lifeLike: true,
+	autoStart: false
+});
+
+const thirdInstance = new TypeIt("#element3", {
+	strings: ["Projects", "Recent Work", "All The Things..."],
+	breakLines: false,
+	nextStringDelay: 3000,
+	loop: false,
+	startDelay: 500,
+	lifeLike: true,
+	autoStart: false
+});
+
+const fourthInstance = new TypeIt("#element4", {
+	strings: ["Contact Me", "Let's Talk..."],
+	breakLines: false,
+	nextStringDelay: 3000,
+	loop: false,
+	startDelay: 500,
+	lifeLike: true,
+	autoStart: false
+});
+
+$(window).scroll(function() {
+   var hT = $('.scroll-to').offset().top,
+       hH = $('.scroll-to').outerHeight(),
+       wH = $(window).height(),
+       wS = $(this).scrollTop();
+	   if (wS > (hT+hH-wH) && (hT > wS) && (wS+wH > hT+hH)){
+	   }
 });
