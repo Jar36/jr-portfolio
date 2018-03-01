45884ea68ed89e12db4641bb342854c828ff644b
Joseph Romero <jromero361980@gmail.com>
2018-03-01 01:27:54 -0700

    redesign layout

--- js/main.js
+++ js/main.js
@@ -81,11 +81,48 @@ const fourthInstance = new TypeIt("#element4", {
 	autoStart: false
 });
 
-$(window).scroll(function() {
+/*$(window).scroll(function() {
    var hT = $('.scroll-to').offset().top,
        hH = $('.scroll-to').outerHeight(),
        wH = $(window).height(),
        wS = $(this).scrollTop();
 	   if (wS > (hT+hH-wH) && (hT > wS) && (wS+wH > hT+hH)){
 	   }
-});
+});*/
+
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+      &&
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
